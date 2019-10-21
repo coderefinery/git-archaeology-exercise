b3f33c5dfc8081eef746cf2d4c59fab2095d7c2e
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2019-10-21 16:40:22 +0200

    add first step to read input file

---
+++ wordcount.py
@@ -0,0 +1,14 @@
+import sys
+
+def load_text(filename):
+    """
+    Load lines from a plain-text file and return these as a list, with
+    trailing newlines stripped.
+    """
+    with open(filename, "r") as input_fd:
+        lines = input_fd.read()
+    return lines
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    lines = load_text(input_file)
