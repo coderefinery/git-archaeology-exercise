bba58cb3e46d541db466a57a6705baf8241070b4
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2019-10-21 18:12:53 +0200

    filter by minimum word length

--- wordcount.py
+++ wordcount.py
@@ -33,11 +33,23 @@ def word_count_dict_to_tuples(counts, decrease=True):
     return sorted(list(counts.items()), key=lambda key_value: key_value[1],
                   reverse=decrease)
 
+def filter_word_counts(counts, min_length=1):
+    """
+    Given a list of (word, count) tuples, create a new list with only
+    those tuples whose word is >= min_length.
+    """
+    stripped = []
+    for (word, count) in counts:
+        if len(word) >= min_length:
+            stripped.append((word, count))
+    return stripped
+
 
 def word_count(input_file, output_file, min_length=1):
     lines = load_text(input_file)
     counts = calculate_word_counts(lines)
     sorted_counts = word_count_dict_to_tuples(counts)
+    sorted_counts = filter_word_counts(sorted_counts, min_length)
     with open(output_file, 'w') as output:
         for count in sorted_counts:
             output.write("%s\n" % " ".join(str(c) for c in count))
